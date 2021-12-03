756af4b84700825ef5e5740c1e90a860999db594
Richard Barnes <rbarnes2@hotmail.co.uk>
2021-12-03 16:20:55 +0000

    feat(auth.ts, index.ts): jWT tokens now implemented and working correctly
    JWT now working, has been tested on apollo server. object id now fixed using toString() method.

--- index.ts
+++ index.ts
@@ -1,16 +1,19 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./graphql/typeDefs/typeDefs');
 const resolvers = require('./graphql/resolvers/resolvers');
-const PORT = process.env.PORT || 4000;
+require('dotenv').config();
+const PORT = process.env.PORT;
+console.log(PORT);
+import { getUserFromToken, createToken } from './graphql/resolvers/auth';
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  // context({ req }) {
-  //   const token = req.headers.authorization;
-  //   const user = getUserFromToken(token);
-  //   return { ...db, user, createToken };
-  // },
+  async context({ req }) {
+    const token = req.headers.authorization?.split(' ')[1];
+    const user = await getUserFromToken(token);
+    return { user, createToken };
+  },
 });
 
 server.listen(PORT).then(() => {
